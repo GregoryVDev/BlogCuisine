6b0fc997f026ddefa8873d1a4c7fe22de4f79c59
GregoryVDev <arexo@hotmail.fr>
2024-08-30 21:52:18 +0200

    Added bootstrap

---
+++ src/inscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/inscription.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <main>
+    </main>
+
+</body>
+
+</html>
