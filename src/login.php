11986765184bda3cb0f2d75db35f89bae2e5e8d5
GregoryVDev <arexo@hotmail.fr>
2024-09-02 21:15:54 +0200

    Added property  main for login.php file

--- src/login.php
+++ src/login.php
@@ -4,11 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Connexion</title>
 </head>
 
 <body>
-
+    <main>
+        <form method="POST">
+            <img src="./img/logos/gregory-trans.png" alt="Logo du site">
+            <h1>Connexion</h1>
+            <div class="container-email">
+                <label for="email">Email :</label>
+                <input type="email" class="form-input" name="email" id="email" placeholder="Email">
+            </div>
+            <div class="container-password">
+                <label for="password">Mot de passe :</label>
+                <input type="password" class="form-input" name="pass" id="pass" placeholder="Mot de passe" required>
+            </div>
+            <button type="submit" class="connexion-button">Se connecter</button>
 </body>
 
 </html>
