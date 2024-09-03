bba8fbea3021be6753918386677135937704b1c7
GregoryVDev <arexo@hotmail.fr>
2024-09-03 14:43:23 +0200

    Added var color4 and added navbar in header.php, added icons for navbar

--- src/template/header.php
+++ src/template/header.php
@@ -10,13 +10,19 @@
 </head>
 
 <body>
+    <div class="navbar-top">
+        <img src="" alt="Dashboard">
+        <img src="" alt="Dark mode">
+        <img src="" alt="Connexion">
+        <img src="" alt="Favoris">
+    </div>
     <header>
         <nav>
             <ul>
                 <li><a href="#home">Accueil</a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#about">A propos</a></li>
+                <li><a href="#category">Catégories</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     </header>
