1434d7b90bf542f491e84dcd3d2da128bf4484c3
GregoryVDev <arexo@hotmail.fr>
2024-09-03 14:10:31 +0200

    Added html structur in index.php and header.php and created folder template and css for the header and footer

--- src/template/header.php
+++ src/template/header.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grég & Élise</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <title>Grég et Élise</title>
 </head>
 
 <body>
