6203f56d963ef612cc82fc200f8acd50edab6140
GregoryVDev <arexo@hotmail.fr>
2024-09-04 11:18:15 +0200

    Removed header.css and added style for footer with newletter

---
+++ src/template/footer.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <div class="footer-top"></div>
+        <div class="container-ul">
+            <div class="container-accueil">
+                <ul>
+                    <h3>Accueil</h3>
+                    <li><a href="#">Présentation</a></li>
+                    <li><a href="#">Catégories</a></li>
+                    <li><a href="#">Derniers articles</a></li>
+                </ul>
+            </div>
+
+            <div class="container-entrees">
+                <ul>
+                    <h3>Entrées</h3>
+                    <li><a href="#">Froides</a></li>
+                    <li><a href="#">Chaudes</a></li>
+                </ul>
+            </div>
+            <div class="container-plats">
+                <ul>
+                    <h3>Plats</h3>
+                    <li><a href="#">Viandes</a></li>
+                    <li><a href="#">Poissons</a></li>
+                </ul>
+            </div>
+            <div class="container-desserts">
+                <ul>
+                    <h3>Desserts</h3>
+                    <li><a href="#">Froids</a></li>
+                    <li><a href="#">Chauds</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="newsletter">
+            <label for="newsletter">Newsletter</label>
+            <input type="email" name="newsletter" id="newsletter">
+        </div>
+    </footer>
+
+</body>
+
+</html>
--- src/template/header.php
+++ src/template/header.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="./css/header.css">
-    <title>Grég et Élise</title>
-</head>
-
-<body>
+<header>
     <div class="navbar-top">
         <div class="container-icons">
             <a href="#"><img src="../img/logos/dashboard.svg" alt="Dashboard"></a>
@@ -21,19 +10,15 @@
             <a href="#"><img src="../img/logos/moon.svg" alt="Dark mode"></a>
         </div>
     </div>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#home">Accueil</a></li>
-                <span class="circle"></span>
-                <li><a href="#about">A propos</a></li>
-                <span class="circle"></span>
-                <li><a href="#category">Catégories</a></li>
-                <span class="circle"></span>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-</body>
-
-</html>
+    <nav>
+        <ul>
+            <li><a href="#home">Accueil</a></li>
+            <span class="circle"></span>
+            <li><a href="#about">A propos</a></li>
+            <span class="circle"></span>
+            <li><a href="#category">Catégories</a></li>
+            <span class="circle"></span>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
