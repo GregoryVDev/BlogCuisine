4f421cd87c9198e1e75fa94f713d0475a6613d10
GregoryVDev <arexo@hotmail.fr>
2024-09-19 18:21:50 +0200

    Added DOCTYPE and header.php in gregory.php

---
+++ src/gregory.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <title>Onglet - Grégory</title>
+</head>
+
+<body>
+    <?php require_once("./template/header.php") ?>
+</body>
+
+</html>
