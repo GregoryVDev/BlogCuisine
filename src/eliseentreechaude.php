fea673262e4f877022ff12f6673ba2fda73ceca9
GregoryVDev <arexo@hotmail.fr>
2024-09-26 15:16:11 +0200

    Edited the php files entréechaude.php and their CSS, and added HTML structure to the files eliseentreechaude.php and created files gregentreechaude and entreegreg.css

---
+++ src/eliseentreechaude.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/entreeelise.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <title>Entrées Chaudes - Elise</title>
+</head>
+
+<body>
+    <?php require_once("./template/header.php") ?>
+    <main>
+        <section id="illustration-entree"></section>
+    </main>
+
+</body>
+
+</html>
