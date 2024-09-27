84b4b9d9e0ecdb04d38bbe9b7d86f52fc2a700f5
GregoryVDev <arexo@hotmail.fr>
2024-09-27 20:22:45 +0200

    Edited name php file and css file

--- src/category.php
+++ src/category.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="./css/elise.css">
+    <link rel="stylesheet" href="./css/category.css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/fonts.css">
     <title>Onglet - Élise</title>
