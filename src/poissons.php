fa466ea9391744fcfd7bba6d68d1918b76858f49
GregoryVDev <arexo@hotmail.fr>
2024-09-28 15:09:29 +0200

    Added id illustration with the fish background

--- src/poissons.php
+++ src/poissons.php
@@ -14,7 +14,7 @@
 <body>
     <?php require_once("./template/header.php") ?>
     <main>
-        <section id="illustration-entree"></section>
+        <section id="illustration-poisson"></section>
         <section id="articles">
             <h2>Poissons</h2>
 
