79819aba17fa4f96d80d54319968d69e1fc73df9
GregoryVDev <arexo@hotmail.fr>
2024-09-30 14:34:17 +0200

    Added the code for the detail file with a section and an article with the css

---
+++ src/detail.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/recettes.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <title>Plat</title>
+</head>
+
+<body>
+    <?php require_once("./template/header.php") ?>
+    <main>
+        <section class="detail">
+            <article class="detail-article">
+                <div class="container-detail">
+                    <a href="#">Plats</a>
+                    <a href="#">Viande</a>
+                </div>
+                </div>
+                <h2>Boeuf Bourguignon</h2>
+                <div class="credit-detail">
+                    <p><span class="credit">par :</span> Grégory</p>
+                    <p><span class="credit">posté :</span> 20 août 2024</p>
+                    <p><span class="credit">tags :</span> <a href="#">Plats</a>, <a href="#">Viande</a></p>
+
+                </div>
+            </article>
+        </section>
+    </main>
+    <?php require_once("./template/footer.php") ?>
+
+
+</body>
+<script src="./js/plats.js"></script>
+
+</html>
