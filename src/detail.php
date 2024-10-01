de471d1d06e6268f47116ea9ce565043da3ded6c
GregoryVDev <arexo@hotmail.fr>
2024-10-01 18:48:02 +0200

    Edited the div with the figure and figcaption

--- src/detail.php
+++ src/detail.php
@@ -27,21 +27,22 @@
                     <p><span class="credit">posté :</span> 20 août 2024</p>
                     <p><span class="credit">tags :</span> <a href="#">Plats</a>, <a href="#">Viande</a></p>
                 </div>
-                <div class="img-container">
+                <figure class="img-container">
                     <img src="./img/plats/boeuf-bourguignon.jpg" alt="Boeuf Bourguignon" class="image-plat">
-                </div>
-                <p class="text-content">Le bœuf bourguignon est un plat traditionnel français, originaire de la région Bourgogne...</p>
+                    <figcaption class="text-content">Le bœuf bourguignon est un plat traditionnel français, originaire de la région Bourgogne...</figcaption>
+                </figure>
+
                 <div class="container-info">
                     <div class="left-info">
-                        <h3>Temps de préparation :</h3>
+                        <h3>Temps de préparation</h3>
                         <p>20 minutes</p>
                     </div>
                     <div class="middle-info">
-                        <h3>Temps de cuisson :</h3>
+                        <h3>Temps de cuisson</h3>
                         <p>40 minutes</p>
                     </div>
                     <div class="right-info">
-                        <h3>Personnes :</h3>
+                        <h3>Personnes</h3>
                         <p>4 personnes</p>
                     </div>
                 </div>
