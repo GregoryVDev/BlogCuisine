9c510672d6f39303086be7471393a181988b67f3
GregoryVDev <arexo@hotmail.fr>
2024-10-08 19:34:32 +0200

    Edited path for css link

--- src/backoffice/ajoutarticle.php
+++ src/backoffice/ajoutarticle.php
@@ -4,11 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/recettes.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fonts.css">
     <title>Backoffice</title>
 </head>
 
 <body>
-    <?php require_once("./src/template/header.php") ?>
+    <?php require_once("../template/header.php") ?>
 
 </body>
 
