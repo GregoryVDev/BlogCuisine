a09b285b7f0d42fef1c595cb6acd9349a0756ac9
GregoryVDev <arexo@hotmail.fr>
2024-10-10 23:15:12 +0200

    Added icons for the navbar backoffice

---
+++ src/backoffice/template/header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="navbar-top">
+        <div class="container-icons">
+            <a href="#"><img src="../img/logos/dashboard.svg" alt="Panel Admin"></a>
+            <div class="dr"></div>
+            <a href="#"><img src="../img/logos/user.svg" alt="Se connecter"> </a>
+            <div class="dr"></div>
+            <a href="#"><img src="../img/logos/heart.svg" alt="Ajouté au favoris"></a>
+            <div class="dr"></div>
+            <a href="#"><img src="../img/logos/moon.svg" alt="Activer le mode sombre"></a>
+        </div>
+    </div>
+
+</header>
