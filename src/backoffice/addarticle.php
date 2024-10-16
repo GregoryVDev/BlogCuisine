3a923d391455c669a374ba947e2a1e707bbde831
GregoryVDev <arexo@hotmail.fr>
2024-10-16 21:09:23 +0200

    Removed the margin for container tags

--- src/backoffice/addarticle.php
+++ src/backoffice/addarticle.php
@@ -16,82 +16,78 @@
     <?php require_once("../backoffice/template/header.php") ?>
     <section class="illust-addarticle"></section>
     <section class="formulaire">
-        <form method="POST">
-            <div class="container">
-                <h3>Ajouter un article</h3>
-                <form action="#" method="post" enctype="multipart/form-data">
-                    <div class="left-section">
-                        <div class="container-prenom">
-                            <label for="prenom">Prénom :</label>
-                            <input type="text" id="prenom" name="firstname" placeholder="Prénom">
-                        </div>
-                        <div class="container-title">
-                            <label for="titre">Titre :</label>
-                            <input type="text" id="titre" name="title" placeholder="Titre">
-                        </div>
-                        <div class="container-categories">
-                            <label for="categorie">Catégorie :</label>
-                            <select id="categorie" name="category">
-                                <option value="">--Choisir une catégorie--</option>
-                                <option value="">Entrées / Apéro</option>
-                                <option value="">Plats</option>
-                                <option value="">Desserts</option>
-                            </select>
-                        </div>
-                        <div class="container-tags-form">
-                            <label for="tags">Tags :</label>
-                            <select id="tags" name="tags">
-                                <option value="">--Choisir des tags--</option>
-                                <option value="">Entrées Froides</option>
-                                <option value="">Entrées Chaudes</option>
-                                <option value="">Apéros</option>
-                                <option value="">Viandes</option>
-                                <option value="">Marins</option>
-                                <option value="">Féculents</option>
-                                <option value="">Desserts Froids</option>
-                                <option value="">Desserts Chauds</option>
-                            </select>
-                        </div>
-                        <div class="container-coocking">
-                            <label for="temps_cuisson">Temps cuisson (minutes) :</label>
-                            <input type="text" id="temps_cuisson" name="coocking" placeholder="Temps de cuisson en minutes">
-                        </div>
-                        <div class="container-preparation">
-                            <label for="temps_preparation">Temps préparation (minutes) :</label>
-                            <input type="text" id="temps_preparation" name="preparation" placeholder="Temps de préparation en minutes">
-                        </div>
-                        <div class="container-personnes">
-                            <label for="personnes">Personnes :</label>
-                            <select id="personnes" name="number">
-                                <option value="">--Choisir le nombre de personne--</option>
-                                <option value="">2</option>
-                                <option value="">4</option>
-                                <option value="">6</option>
-                                <option value="">8</option>
-                            </select>
-                        </div>
+        <div class="container">
+            <h3>Ajouter un article</h3>
+            <form action="#" method="post" enctype="multipart/form-data">
+                <div class="left-section">
+                    <div class="container-prenom">
+                        <label for="prenom">Prénom :</label>
+                        <input type="text" id="prenom" name="firstname" placeholder="Prénom">
                     </div>
-
-                    <div class="right-section">
-                        <label for="description">Description :</label>
-                        <textarea id="description" name="description" placeholder="Description"></textarea>
-
-                        <label for="ingredients">Ingrédients :</label>
-                        <textarea id="ingredients" name="ingredients" placeholder="Ingrédients"></textarea>
-
-                        <label for="preparation">Préparation :</label>
-                        <textarea id="preparation" name="preparation" placeholder="Instruction de la préparation"></textarea>
-
-                        <label id="uploadLabel" for="image">Uploader une photo</label>
-                        <input type="file" id="image" style="display: none;">
-                        <img id="previewImage" src="#" alt="Aperçu de l'image">
+                    <div class="container-title">
+                        <label for="titre">Titre :</label>
+                        <input type="text" id="titre" name="title" placeholder="Titre">
                     </div>
+                    <div class="container-categories">
+                        <label for="categorie">Catégorie :</label>
+                        <select id="categorie" name="category">
+                            <option value="">--Choisir une catégorie--</option>
+                            <option value="">Entrées / Apéro</option>
+                            <option value="">Plats</option>
+                            <option value="">Desserts</option>
+                        </select>
+                    </div>
+                    <div class="container-tags-form">
+                        <label for="tags">Tags :</label>
+                        <select id="tags" name="tags">
+                            <option value="">--Choisir des tags--</option>
+                            <option value="">Entrées Froides</option>
+                            <option value="">Entrées Chaudes</option>
+                            <option value="">Apéros</option>
+                            <option value="">Viandes</option>
+                            <option value="">Marins</option>
+                            <option value="">Féculents</option>
+                            <option value="">Desserts Froids</option>
+                            <option value="">Desserts Chauds</option>
+                        </select>
+                    </div>
+                    <div class="container-coocking">
+                        <label for="temps_cuisson">Temps cuisson (minutes) :</label>
+                        <input type="text" id="temps_cuisson" name="coocking" placeholder="Temps de cuisson en minutes">
+                    </div>
+                    <div class="container-preparation">
+                        <label for="temps_preparation">Temps préparation (minutes) :</label>
+                        <input type="text" id="temps_preparation" name="preparation" placeholder="Temps de préparation en minutes">
+                    </div>
+                    <div class="container-personnes">
+                        <label for="personnes">Personnes :</label>
+                        <select id="personnes" name="number">
+                            <option value="">--Choisir le nombre de personne--</option>
+                            <option value="">2</option>
+                            <option value="">4</option>
+                            <option value="">6</option>
+                            <option value="">8</option>
+                        </select>
+                    </div>
+                </div>
 
-                    <button type="submit">Envoyer</button>
-                </form>
-            </div>
+                <div class="right-section">
+                    <label for="description">Description :</label>
+                    <textarea id="description" name="description" placeholder="Description"></textarea>
 
-        </form>
+                    <label for="ingredients">Ingrédients :</label>
+                    <textarea id="ingredients" name="ingredients" placeholder="Ingrédients"></textarea>
+
+                    <label for="preparation">Préparation :</label>
+                    <textarea id="preparation" name="preparation" placeholder="Instruction de la préparation"></textarea>
+
+                    <label id="uploadLabel" for="image">Uploader une photo</label>
+                    <input type="file" id="image" style="display: none;">
+                    <img id="previewImage" src="#" alt="Aperçu de l'image">
+                </div>
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
     </section>
 
 
