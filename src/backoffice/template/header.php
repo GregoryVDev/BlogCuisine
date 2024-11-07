9196cf22c41fbddb821b229f8b2b8b4c1660eab9
GregoryVDev <arexo@hotmail.fr>
2024-11-07 13:18:49 +0100

    Added editarticle file and edited the header, removed the icons darkmode and favoris

--- src/backoffice/template/header.php
+++ src/backoffice/template/header.php
@@ -4,10 +4,6 @@
             <a href="#"><img src="../img/logos/dashboard.svg" alt="Panel Admin"></a>
             <div class="dr"></div>
             <a href="#"><img src="../img/logos/user.svg" alt="Se connecter"> </a>
-            <div class="dr"></div>
-            <a href="#"><img src="../img/logos/heart.svg" alt="Ajouté au favoris"></a>
-            <div class="dr"></div>
-            <a href="#"><img src="../img/logos/moon.svg" alt="Activer le mode sombre"></a>
         </div>
     </div>
     <nav>
