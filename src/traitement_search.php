c5d48aefd35c54a6b182b007eb859182a07d6de7
GregoryVDev <arexo@hotmail.fr>
2024-12-18 16:44:27 +0100

    Removed the commentary php code

--- src/traitement_search.php
+++ src/traitement_search.php
@@ -1,17 +1,17 @@
 <?php
 
-// if ($_SERVER["REQUEST_METHOD"] === "GET") {
-//     $_GET["search"] = htmlspecialchars(($_GET["search"]));
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $_GET["search"] = htmlspecialchars(($_GET["search"]));
 
-//     $search = strip_tags($_GET["search"]);
+    $search = strip_tags($_GET["search"]);
 
-//     require_once("./connect.php");
+    require_once("./connect.php");
 
-//     $search = "%$search%";
+    $search = "%$search%";
 
-//     $sql = "SELECT article_id FROM article WHERE title LIKE :search";
-//     $query = $db->prepare($sql);
-//     $query->bindValue(":search", $search);
-//     $query->execute();
-//     $result = $query->fetchall();
-// }
+    $sql = "SELECT article_id FROM article WHERE title LIKE :search";
+    $query = $db->prepare($sql);
+    $query->bindValue(":search", $search);
+    $query->execute();
+    $result = $query->fetchall();
+}
